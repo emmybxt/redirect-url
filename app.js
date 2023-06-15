c2c193d59fb36dcf08dd4f18f4e23f0b4c3471e5
Nwaobi Emeka <chukwuemeka.n@ssoftcom.xyz>
2023-06-15 15:59:11 +0100

    firstcommit

---
+++ app.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const express = require('express');
+const { json } = require('body-parser');
+
+
+const app = express();
+
+app.use(json({ limit: "5mb" }));
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        status: true,
+        success: true
+    });
+});
+app.get('/url/', (req, res) => {
+
+    const { url } = req.query;
+
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url,
+      headers: {
+        'Referer': 'https://www.thenetnaija.net/videos/series/18878-the-big-d/season-1/episode-1'
+      }
+    };
+    
+    let redirecturl = "";
+    
+    async function makeRequest() {
+      try {
+        const response = await axios.request(config);
+        const redirectedUrl = response.request.res.responseUrl;
+        redirecturl = redirectedUrl;
+    
+        console.log(redirectedUrl);
+    
+        // Return the response inside the makeRequest function
+        return res.status(200).json({
+          success: true,
+          data: redirecturl
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    
+    makeRequest();
+    
+
+});
+
+
+app.listen(3000, () => {
+    console.log("App started on port 3000");
+});
